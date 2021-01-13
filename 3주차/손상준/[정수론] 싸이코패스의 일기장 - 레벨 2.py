83140a2569a60ba8e4461b6029c7b729caef9955
onshore <onshorecoding9@gmail.com>
2021-01-13 15:38:12 +0900

    210113 code fixed

--- 3주차/손상준/[정수론] 싸이코패스의 일기장 - 레벨 2.py
+++ 3주차/손상준/[정수론] 싸이코패스의 일기장 - 레벨 2.py
@@ -13,19 +13,19 @@ for i in diary:
         new_idx = P.find(str(i)) - 4
         print(P[new_idx], end = "")
 
+# for i in diary:
+#     inew _ idx = P. find(str(i))
+# sangjourneyuy
+# s
 
+# note = input()
 
+# alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
---
+# ans = ''
 
-note = input()
-
-alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-ans = ''
-
-for i in range(len(note)):
-    for j in range(len(alp)):
-        if note[i] == alp[j]:
-            ans += alp[j-4]
-print(ans)
+# for i in range(len(note)):
+#     for j in range(len(alp)):
+#         if note[i] == alp[j]:
+#             ans += alp[j-4]
+# print(ans)
