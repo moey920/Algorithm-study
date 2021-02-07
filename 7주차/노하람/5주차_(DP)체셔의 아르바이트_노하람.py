deb356b67f0631e32a2b5d57262e48ad4ac887bb
rohharam <moey920@naver.com>
2021-02-08 03:23:44 +0900

    삼각사각 풀이, 다른 것은 제대로 못품

---
+++ 7주차/노하람/5주차_(DP)체셔의 아르바이트_노하람.py
@@ -0,0 +1,20 @@
+# N, M = map(int, input().split())
+"""
+풀이 :
+
+"""
+
+
+# 튜터님 코드
+N, M = list(map(int, input().split(' ')))
+dp = [0 for i in range(N + 1)]
+dp[0] = 1
+ 
+for i in range(1, N+1):
+    dp[i] += dp[i - 1]
+    if i >= M:
+        dp[i] += dp[i - M]
+    # print(dp[i])
+    dp[N] %= 1000007
+ 
+print(dp[N])
