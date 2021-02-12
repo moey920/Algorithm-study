eb4cf61a9fa4ec25b640728c26b2f842a87e8ccb
인선 강 <sunink1@naver.com>
2021-02-12 17:00:24 +0900

    오류 해결ㅠㅠㅠㅠㅠ

--- 7주차/[BFS]유류창고 테러사건.py
+++ 7주차/[BFS]유류창고 테러사건.py
@@ -1,11 +1,56 @@
 from collections import deque
 from itertools import combinations
 
+
+def bfs(lst):
+    visited=[[0]*n for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            if warehouse[i][j]==1 or warehouse[i][j]==2:
+                visited[i][j]=1
+    
+    
+    q=deque()
+    for i in lst:
+        visited[i[0]][i[1]]=1
+        q.append(i)
+    
+
+    while q:
+        x, y, time=q.popleft()
+        
+        time+=1
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if 0<=nx<n and 0<=ny<n:
+                if warehouse[nx][ny]==0 and visited[nx][ny]==0:
+                    visited[nx][ny]=1
+                    q.append([nx, ny, time])
+                elif warehouse[nx][ny]==1 and visited[nx][ny]==0:
+                    visited[nx][ny]=1
+                elif warehouse[nx][ny]==2 and visited[nx][ny]==0:
+                    visited[nx][ny]=1
+    
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j]==0:
+                return (-1)
+    
+    return (time-1)
+
+
+
+
+
+
+
 n, m = map(int, input().split())
 
 warehouse=[list(map(int, input().split())) for _ in range(n)]
 
-
+time=0
 
 wherefire=[]
 
@@ -13,59 +58,25 @@ wherefire=[]
 for i in range(n):
     for j in range(n):
         if warehouse[i][j]==2:
-            wherefire.append([i,j])
+            wherefire.append([i,j, time])
             
 canfire=list(combinations(wherefire,m))
 
-
-
 dx=[0,0,1,-1]
 dy=[1,-1,0,0]
-
-def bfs(lst):
-    temp=[]
-    time=0
-    q=deque()
-    for i in lst:
-        q.append([i,time])
-    while q:
-        temp=q.popleft()
-        x=temp[0][0]
-        y=temp[0][1]
-        time=temp[1]
-        visited[x][y]=2
-        
-        
-        
-        if visited[x][y]==2:
-            time+=1
-            for i in range(4):
-                nx=x+dx[i]
-                ny=y+dy[i]
-                
-                if 0<=nx<n and 0<=ny<n:
-                    
-                    if warehouse[nx][ny]==0 and visited[nx][ny]==0:
-                        visited[nx][ny]=2
-                        q.append([[nx, ny],time])
-                    elif warehouse[nx][ny]==1 and visited[nx][ny]==0:
-                        visited[nx][ny]=1
-                    elif warehouse[nx][ny]==2 and visited[nx][ny]==0:
-                        visited[nx][ny]=2
-    for i in range(n):
-        for j in range(n):
-            if visited[i][j]==0 and warehouse[i][j]==0:
-                return(-1)
-
-    return(time-1) 
-
 res=[]
+res2=[]
+check=0
+
+for i in canfire:
+    res.append(bfs(i))
 
 for i in range(len(canfire)):
-    visited=[[0]*n for _ in range(n)]
-    res.append(bfs(canfire[i]))
+    if res[i]!=-1:
+        check=1
+        res2.append(res[i])
 
-if not res:
-    print(-1)   
+if check==1:
+    print(min(res2))
 else:
-    print(min(res))
+    print(-1)
