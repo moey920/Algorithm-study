29188e9bc80a1eae0d3fae5206b2a13e35dc3dbf
인선 강 <sunink1@naver.com>
2021-02-12 15:25:07 +0900

    데굴데굴 눈덩이

---
+++ 7주차/[BFS]데굴데굴 눈덩이!.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+
+def bfs(pos):
+    res=[]
+    q=deque()
+    q.append(pos)
+    
+    while q:
+        x, y, cnt=q.popleft()
+        cnt+=1
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if 0<=nx<n and 0<=ny<n:
+                if snowmap[nx][ny]>=snowmap[x][y]:
+                    q.append((nx, ny, cnt))
+
+    return (cnt)
+
+
+
+
+n=int(input())
+snowmap=[list(map(int, input().split())) for _ in range(n)]
+visited=[[0]*n for _ in range(n)]
+cnt=0
+res=[]
+
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
+
+for i in range(n):
+    for j in range(n):
+        res.append(bfs((i,j,cnt)))
+        
+print(max(res))
