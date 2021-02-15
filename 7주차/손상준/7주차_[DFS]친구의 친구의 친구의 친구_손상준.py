a4ffada9b9911bf977dd613046cbadb40fa9e96b
onshore <onshorecoding9@gmail.com>
2021-02-15 21:26:12 +0900

    friend question solved

--- 7주차/손상준/7주차_[DFS]친구의 친구의 친구의 친구_손상준.py
+++ 7주차/손상준/7주차_[DFS]친구의 친구의 친구의 친구_손상준.py
@@ -1,20 +1,3 @@
-#5번 case 시간초과를 해결하지 못함
-# def dfs(node):
-#     global cnt 
-#     cnt += 1
-#     visited[node] = True 
-
-#     if cnt == 5: #여기 위치에서, return 값을 True로 하여 탐색을 종료하려고하는데 왜 안되는지 잘 모르겠습니다 
-#         return True  
-
-#     for i in range(N+1): 
-#         if visited[i] == False: #탐색된 노드 예외처리
-#             if rel[node][i] == 1 or rel[i][node] == 1: #연결관계 노드 탐색
-#                 dfs(i)
-        
-#     visited[node] = False
-#     cnt -= 1
-
 from sys import stdin, setrecursionlimit 
 
 setrecursionlimit(1000000000)
@@ -30,9 +13,8 @@ def dfs(node):
         if visited[i] == False: #탐색된 노드 예외처리
             if rel[node][i] == 1 or rel[i][node] == 1: #연결관계 노드 탐색
                 dfs(i)
-    
-    if cnt == 5: #노드 깊이가 5일 경우, True 반환하여 friend()로 전달 (탐색이 cnt > 5 를 넘어 끝까지 재귀가 돌아 해결되지 않을 것으로 생각합니다, for문 위 아래로 조건을 넣어도 해결 안됨)
-        return True
+                if cnt == 5:
+                    return True
     
     #초기화
     visited[node] = False 
